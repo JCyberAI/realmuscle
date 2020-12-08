b88acfa3d0511d58bf39b763a1bf05d0c149a485
Eamon McHugh <eamonmchugh2@gmail.com>
2020-12-08 15:54:54 -0500

    links to materialize and fontawesome. added navebar and burger js

---
+++ script/main.js
@@ -0,0 +1,13 @@
+//Nav Bar Burger Sript
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+function myFunction(x) {
+    x.classList.toggle("change");
+}
