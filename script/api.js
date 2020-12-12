addc85e4af4502a750624e8a16d814a013c07c33
Qi Feng <qi.feng86@gmail.com>
2020-12-12 11:33:47 -0500

    added reference to js files and moved remove button code to main.js file

--- script/api.js
+++ script/api.js
@@ -116,29 +116,3 @@ $("#addButton").click(function () {
 
     });
 });
-
-$("#Removebutton1").click(function Clearcard1Fields() {
-
-    document.getElementById("address1").textContent = "";
-    document.getElementById("price1").textContent = "";
-    document.getElementById("size1").textContent = "";
-    document.getElementById("rooms1").textContent = "";
-    document.getElementById("baths1").textContent = "";
-    document.getElementById("propertyimage1").innerHTML = "";
-    document.getElementById("mapContainer1").innerHTML = "";
-
-});
-
-
-$("#Removebutton2").click(function Clearcard2Fields() {
-
-    document.getElementById("address2").textContent = "";
-    document.getElementById("price2").textContent = "";
-    document.getElementById("size2").textContent = "";
-    document.getElementById("rooms2").textContent = "";
-    document.getElementById("baths2").textContent = "";
-    document.getElementById("propertyimage2").innerHTML = "";
-    document.getElementById("mapContainer2").innerHTML = "";
-
-
-});
--- script/main.js
+++ script/main.js
@@ -1,13 +1,25 @@
-//Nav Bar Burger Sript
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
+$("#Removebutton1").click(function Clearcard1Fields() {
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-}
-function myFunction(x) {
-    x.classList.toggle("change");
-}
+    document.getElementById("address1").textContent = "";
+    document.getElementById("price1").textContent = "";
+    document.getElementById("size1").textContent = "";
+    document.getElementById("rooms1").textContent = "";
+    document.getElementById("baths1").textContent = "";
+    document.getElementById("propertyimage1").innerHTML = "";
+    document.getElementById("mapContainer1").innerHTML = "";
+
+});
+
+
+$("#Removebutton2").click(function Clearcard2Fields() {
+
+    document.getElementById("address2").textContent = "";
+    document.getElementById("price2").textContent = "";
+    document.getElementById("size2").textContent = "";
+    document.getElementById("rooms2").textContent = "";
+    document.getElementById("baths2").textContent = "";
+    document.getElementById("propertyimage2").innerHTML = "";
+    document.getElementById("mapContainer2").innerHTML = "";
+
+
+});
